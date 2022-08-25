b51223cc1934e82551de92fa49550e20aa1db3d3
Bianca Fota <fotabianca6@gmail.com>
2022-08-25 11:07:51 +0300

    My first commit message

---
+++ main2.js
@@ -0,0 +1,25 @@
+/*const age = 17;
+if (age < 18) {
+    console.log("You are not allowed here");
+} else if (age < 40) {
+    console.log("Ypu get a 🍷🍷");
+} else {
+    console.log("you get a 🥃")
+}*/
+
+/*const firma = " Hyundai";
+
+switch (firma) {
+    case "Dacia":
+        console.log("Romania");
+        break;
+    case "Mercedes":
+        console.log("Germania");
+        break;
+    case " Hyundai":
+        console.log("Japonia");
+}*/
+
+/*while (true) {
+    consolr.log("Cartofi");
+}*/
---
+++ sesiunea1/ses1.js
@@ -0,0 +1,8 @@
+/*const brand = "Mitsubishi";
+const wheels = 4;
+const country_of_origin = "Japan"
+const max_speed = 180;
+const is_on = false;
+
+
+console.log("The" + brand + "originated from" + country_of_origin);*/
